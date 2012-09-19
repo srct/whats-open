716f240db38be424ab542e57eae6a6d7fae736cc
Tyler Hallada <tyler@hallada.net>
2012-09-18 21:26:19 -0400

    Renamed model fields to correct style. Other bug fixes.

--- website/models.py
+++ website/models.py
@@ -5,9 +5,9 @@ import datetime
 class Restaurant(models.Model):
     """Represents a dining location on campus."""
     name = models.CharField(max_length=100)
-    mainSchedule = models.ForeignKey('Schedule',
+    main_schedule = models.ForeignKey('Schedule',
             related_name='restaurant_main')
-    specialSchedules = models.ManyToManyField('Schedule',
+    special_schedules = models.ManyToManyField('Schedule',
             related_name='restaurant_special', null=True, blank=True)
 
     def isOpen(self):
@@ -18,14 +18,20 @@ class Restaurant(models.Model):
         main default schedule.
 
         """
-        today = datetime.datetime.today()
-        for schedule in self.specialSchedules.all():
-            if schedule.dateValidStart <= today <= schedule.dateValidEnd:
-                if schedule.isOpenNow():
-                    return True
-        if self.mainSchedule.isOpenNow():
+        today = datetime.datetime.today().date()
+        # Check special schedules first
+        for schedule in self.special_schedules.all():
+            # Special schedules must have valid_start and valid_end set
+            if schedule.valid_start and schedule.valid_end:
+                if schedule.valid_start <= today <= schedule.valid_end:
+                    if schedule.isOpenNow():
+                        return True
+        if self.main_schedule.isOpenNow():
             return True
         return False
+
+    def __unicode__(self):
+        return self.name
 
 
 class Schedule(models.Model):
@@ -38,50 +44,42 @@ class Schedule(models.Model):
     """
     name = models.CharField(max_length=100)
     # inclusive:
-    dateValidStart = models.DateField(null=True, blank=True)
-    dateValidEnd = models.DateField(null=True, blank=True)
-    monOpen = models.TimeField(null=True, blank=True)
-    monClose = models.TimeField(null=True, blank=True)
-    tueOpen = models.TimeField(null=True, blank=True)
-    tueClose = models.TimeField(null=True, blank=True)
-    wedOpen = models.TimeField(null=True, blank=True)
-    wedClose = models.TimeField(null=True, blank=True)
-    thuOpen = models.TimeField(null=True, blank=True)
-    thuClose = models.TimeField(null=True, blank=True)
-    friOpen = models.TimeField(null=True, blank=True)
-    friClose = models.TimeField(null=True, blank=True)
-    satOpen = models.TimeField(null=True, blank=True)
-    satClose = models.TimeField(null=True, blank=True)
-    sunOpen = models.TimeField(null=True, blank=True)
-    sunClose = models.TimeField(null=True, blank=True)
+    valid_start = models.DateField(null=True, blank=True)
+    valid_end = models.DateField(null=True, blank=True)
+    mon_open = models.TimeField(null=True, blank=True)
+    mon_close = models.TimeField(null=True, blank=True)
+    tue_open = models.TimeField(null=True, blank=True)
+    tue_close = models.TimeField(null=True, blank=True)
+    wed_open = models.TimeField(null=True, blank=True)
+    wed_close = models.TimeField(null=True, blank=True)
+    thu_open = models.TimeField(null=True, blank=True)
+    thu_close = models.TimeField(null=True, blank=True)
+    fri_open = models.TimeField(null=True, blank=True)
+    fri_close = models.TimeField(null=True, blank=True)
+    sat_open = models.TimeField(null=True, blank=True)
+    sat_close = models.TimeField(null=True, blank=True)
+    sun_open = models.TimeField(null=True, blank=True)
+    sun_close = models.TimeField(null=True, blank=True)
 
     def isOpenNow(self):
         """Return true if this schedule is open right now."""
         today = datetime.datetime.today()
         weekday = today.weekday()
-        if weekday == 0:
-            start = self.monOpen
-            end = self.monClose
-        elif weekday == 1:
-            start = self.tueOpen
-            end = self.tueClose
-        elif weekday == 2:
-            start = self.wedOpen
-            end = self.wedClose
-        elif weekday == 3:
-            start = self.thuOpen
-            end = self.thuClose
-        elif weekday == 4:
-            start = self.friOpen
-            end = self.friClose
-        elif weekday == 5:
-            start = self.satOpen
-            end = self.satClose
-        elif weekday == 6:
-            start = self.sunOpen
-            end = self.sunClose
+        days = [
+                (self.mon_open, self.mon_close),
+                (self.tue_open, self.tue_close),
+                (self.wed_open, self.wed_close),
+                (self.thu_open, self.thu_close),
+                (self.fri_open, self.fri_close),
+                (self.sat_open, self.sat_close),
+                (self.sun_open, self.sun_close),
+        ]
+        start, end = days[weekday]
         if (start is not None and end is not None and
                 start <= today.time() <= end):
             return True
         else:
             return False
+
+    def __unicode__(self):
+        return self.name
--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,5 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('website.views',
-    url(r'^$', 'restaurant_grid', name = 'restaurant_grid'),
+    url(r'^$', 'restaurant_grid', name='restaurant_grid'),
 )
-
-
