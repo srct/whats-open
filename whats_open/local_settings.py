9b3aaa75b0b8e2dd1e7e681f5e5e2c5f32b40d9c
Tyler Hallada <tyler@hallada.net>
2012-10-09 16:06:04 -0400

    Adding local settings, normal settings for heroku.

--- whats_open/local_settings.py
+++ whats_open/local_settings.py
@@ -1,6 +1,5 @@
 # Django settings for whats_open project.
 import os
-import dj_database_url
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -13,7 +12,16 @@ MANAGERS = ADMINS
 
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_PATH, '..', 'database.db'),  # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
