5d880c968cb8f2fe893b26c5f92da5b5f30e2ff4
Tyler Hallada <tyler@hallada.net>
2012-10-22 22:49:51 -0400

    Alphabetically sorting the restaurant names.

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from website.models import Restaurant
 from django.shortcuts import render_to_response
+import re
 
 
 def restaurant_grid(request):
@@ -9,6 +10,10 @@ def restaurant_grid(request):
             # Display the grid by location (instead of listing alphabetically)
             pass  # Not implemented yet
     restaurants = Restaurant.objects.all()
+    # Sort the restaurants by alphabetical order ignoring "the" and "a"
+    restaurants = sorted(restaurants,
+            key=lambda r: re.sub('^(the|a) ', '', r.name, count=1,
+            flags=re.IGNORECASE))
     # Restaurants in lists of 4 to easily create rows in template
     restRows = [restaurants[x:x + 4] for x in xrange(0, len(restaurants), 4)]
     return render_to_response('restaurant_grid.html', {'restRows': restRows,
