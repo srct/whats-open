0f3ddfef42fd23874266c05bc83f58a38e70b193
Tyler Hallada <tyler@hallada.net>
2013-01-31 19:29:40 -0500

    Reset the south migration history.
    Major schema change in last commit broke things horribly.

--- website/migrations/0001_initial.py
+++ website/migrations/0001_initial.py
@@ -8,83 +8,95 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Adding model 'BaseModel'
+        db.create_table('website_basemodel', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('last_modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal('website', ['BaseModel'])
+
         # Adding model 'Restaurant'
         db.create_table('website_restaurant', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('basemodel_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['website.BaseModel'], unique=True, primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('mainSchedule', self.gf('django.db.models.fields.related.ForeignKey')(related_name='restaurant_main', to=orm['website.Schedule'])),
+            ('main_schedule', self.gf('django.db.models.fields.related.ForeignKey')(related_name='restaurant_main', to=orm['website.Schedule'])),
         ))
         db.send_create_signal('website', ['Restaurant'])
 
-        # Adding M2M table for field specialSchedules on 'Restaurant'
-        db.create_table('website_restaurant_specialSchedules', (
+        # Adding M2M table for field special_schedules on 'Restaurant'
+        db.create_table('website_restaurant_special_schedules', (
             ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
             ('restaurant', models.ForeignKey(orm['website.restaurant'], null=False)),
             ('schedule', models.ForeignKey(orm['website.schedule'], null=False))
         ))
-        db.create_unique('website_restaurant_specialSchedules', ['restaurant_id', 'schedule_id'])
+        db.create_unique('website_restaurant_special_schedules', ['restaurant_id', 'schedule_id'])
 
         # Adding model 'Schedule'
         db.create_table('website_schedule', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('dateValidStart', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
-            ('dateValidEnd', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
-            ('monOpen', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
-            ('monClose', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
-            ('tueOpen', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
-            ('tueClose', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
-            ('wedOpen', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
-            ('wedClose', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
-            ('thuOpen', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
-            ('thuClose', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
-            ('friOpen', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
-            ('friClose', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
-            ('satOpen', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
-            ('satClose', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
-            ('sunOpen', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
-            ('sunClose', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
+            ('basemodel_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['website.BaseModel'], unique=True, primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('valid_start', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
+            ('valid_end', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
         ))
         db.send_create_signal('website', ['Schedule'])
 
+        # Adding model 'OpenTime'
+        db.create_table('website_opentime', (
+            ('basemodel_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['website.BaseModel'], unique=True, primary_key=True)),
+            ('schedule', self.gf('django.db.models.fields.related.ForeignKey')(related_name='open_times', to=orm['website.Schedule'])),
+            ('start_day', self.gf('django.db.models.fields.IntegerField')()),
+            ('start_time', self.gf('django.db.models.fields.TimeField')()),
+            ('end_day', self.gf('django.db.models.fields.IntegerField')()),
+            ('end_time', self.gf('django.db.models.fields.TimeField')()),
+        ))
+        db.send_create_signal('website', ['OpenTime'])
+
 
     def backwards(self, orm):
+        # Deleting model 'BaseModel'
+        db.delete_table('website_basemodel')
+
         # Deleting model 'Restaurant'
         db.delete_table('website_restaurant')
 
-        # Removing M2M table for field specialSchedules on 'Restaurant'
-        db.delete_table('website_restaurant_specialSchedules')
+        # Removing M2M table for field special_schedules on 'Restaurant'
+        db.delete_table('website_restaurant_special_schedules')
 
         # Deleting model 'Schedule'
         db.delete_table('website_schedule')
 
+        # Deleting model 'OpenTime'
+        db.delete_table('website_opentime')
+
 
     models = {
-        'website.restaurant': {
-            'Meta': {'object_name': 'Restaurant'},
+        'website.basemodel': {
+            'Meta': {'object_name': 'BaseModel'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'mainSchedule': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'restaurant_main'", 'to': "orm['website.Schedule']"}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'website.opentime': {
+            'Meta': {'object_name': 'OpenTime', '_ormbases': ['website.BaseModel']},
+            'basemodel_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['website.BaseModel']", 'unique': 'True', 'primary_key': 'True'}),
+            'end_day': ('django.db.models.fields.IntegerField', [], {}),
+            'end_time': ('django.db.models.fields.TimeField', [], {}),
+            'schedule': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'open_times'", 'to': "orm['website.Schedule']"}),
+            'start_day': ('django.db.models.fields.IntegerField', [], {}),
+            'start_time': ('django.db.models.fields.TimeField', [], {})
+        },
+        'website.restaurant': {
+            'Meta': {'object_name': 'Restaurant', '_ormbases': ['website.BaseModel']},
+            'basemodel_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['website.BaseModel']", 'unique': 'True', 'primary_key': 'True'}),
+            'main_schedule': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'restaurant_main'", 'to': "orm['website.Schedule']"}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'specialSchedules': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'restaurant_special'", 'symmetrical': 'False', 'to': "orm['website.Schedule']"})
+            'special_schedules': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'restaurant_special'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['website.Schedule']"})
         },
         'website.schedule': {
-            'Meta': {'object_name': 'Schedule'},
-            'dateValidEnd': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
-            'dateValidStart': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
-            'friClose': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
-            'friOpen': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'monClose': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
-            'monOpen': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
-            'satClose': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
-            'satOpen': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
-            'sunClose': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
-            'sunOpen': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
-            'thuClose': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
-            'thuOpen': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
-            'tueClose': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
-            'tueOpen': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
-            'wedClose': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
-            'wedOpen': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'})
+            'Meta': {'object_name': 'Schedule', '_ormbases': ['website.BaseModel']},
+            'basemodel_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['website.BaseModel']", 'unique': 'True', 'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'valid_end': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'valid_start': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'})
         }
     }
 
