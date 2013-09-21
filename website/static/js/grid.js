0798836685705d1d00e449412a38f15efdfc8dfb
Renfred Harper <Renfred93@gmail.com>
2013-09-21 19:44:02 -0400

    Fix grid class format

--- website/static/js/grid.js
+++ website/static/js/grid.js
@@ -24,7 +24,7 @@ function construct_grid(filtered_restaurants) {
         }
         // Append the data into the Bootstrap scaffolding
         $('#grid .row').append(
-            '<div class="col-sm-6 col-md-4 col-lg-3 grid-box id="' + restaurant.id + '">\
+            '<div class="col-sm-6 col-md-4 col-lg-3 grid-box" id="' + restaurant.id + '">\
                 <div class="restaurant ' + open_class + '">' + restaurant.name + '</div>\
             </div>'
         );
