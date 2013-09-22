bf6afac7353894d1d44a61bd1e27a49981d1a617
Renfred Harper <Renfred93@gmail.com>
2013-09-22 16:06:21 -0400

    Delete script from document.js

--- website/static/js/document.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var lastClicked = null;
-    $('#info-body').click(function() {
-       $(this).slideUp(350); 
-    });
-    $(document).on('click', '.grid-box', function(){
-        $('#info-body').slideDown(350);
-    });
-});
