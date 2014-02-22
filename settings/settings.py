55372c409a5488a111f63745ac333ab0cbd1cb47
Renfred Harper <renfredxh@gmail.com>
2014-02-22 16:08:10 -0500

    Add management to settings

--- settings/settings.py
+++ settings/settings.py
@@ -127,12 +127,11 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'south',
     'website',
+    'management',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
