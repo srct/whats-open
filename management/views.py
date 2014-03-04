a9f5bffce9eeb32299d1bf0ae6dbe2ec3aa4659f
Ben Waters <bwaters3@gmu.edu>
2014-03-04 03:22:24 +0000

    fixed migrations

--- management/views.py
+++ management/views.py
@@ -2,4 +2,6 @@ from django.http import Http404
 from django.shortcuts import render_to_response
 
 def index(request):
-  return render_to_response('management/index.html')
+    return render_to_response('management/index.html')
+def login(requets):
+    return render_to_response('management/login.html') 
