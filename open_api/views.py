752a3b7335a28187426976dd468fdd4f1d6c2510
Ben Waters <bwaters3@gmu.edu>
2014-03-04 01:09:56 +0000

    going to finish up api

--- open_api/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from website.serializers import OpenTimeSerializers, ScheduleSerializer, FacilitySerializer, CategorySerializer
-
-# Create your views here.
-class OpenTimeViewSet()
-
-class FacilityViewSet():
