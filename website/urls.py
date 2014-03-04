db73f828252ed87391a742a7b6a676b38c414917
Ben Waters <bwaters3@gmu.edu>
2014-03-04 01:51:46 +0000

    commented ot cbv urls for now

--- website/urls.py
+++ website/urls.py
@@ -14,13 +14,13 @@ urlpatterns = patterns('whats_open_site.views',
     url(r'^$', 'facility_grid', name='facility_grid'),
     url(r'^api/', include(router.urls)),
     #facilities open urls
-    url(r'^facilities/$', FacilityListView.as_view(), name='faciliites-list'),
-    url(r'^facilities/(?P<category>)/$', FacilityCategoryListView.as_view(), name='facilities-list-by-cat'),
-    url(r'^facilities/(?P<slug>)/$', FacilityDetailView.as_view(), name='facilities-detail'),
-    url(r'^facilities/(?P<on_campus>)/$', FacilityStatusListView.as_view(), name='facilities-list-by-status'),
+    #url(r'^facilities/$', FacilityListView.as_view(), name='faciliites-list'),
+    #url(r'^facilities/(?P<category>)/$', FacilityCategoryListView.as_view(), name='facilities-list-by-cat'),
+    #url(r'^facilities/(?P<slug>)/$', FacilityDetailView.as_view(), name='facilities-detail'),
+    #url(r'^facilities/(?P<on_campus>)/$', FacilityStatusListView.as_view(), name='facilities-list-by-status'),
 
     #schedules urls
-    url(r'^schedule/(?P<pk>)/$', ScheduleDetailView.as_view(), name='schedule-detail'),
+    #url(r'^schedule/(?P<pk>)/$', ScheduleDetailView.as_view(), name='schedule-detail'),
 
     #opentime urls
     url(r'^open-time/(?P<pk>)/$', OpenTimeDetailView.as_view(), name='open-time-detail'),
