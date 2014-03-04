ac5198a303d5a7c6a12616942e7fc91d16baf5cc
Ben Waters <bwaters3@gmu.edu>
2014-03-04 02:59:44 +0000

    added category to admin)

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Facility, Schedule, OpenTime
+from .models import Facility, Schedule, OpenTime, Category
 
 class OpenTimeAdmin(admin.ModelAdmin):
     pass
@@ -7,7 +7,11 @@ class FacilityAdmin(admin.ModelAdmin):
     pass
 class ScheduleAdmin(admin.ModelAdmin):
     pass
+class CategoryAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(Facility, FacilityAdmin)
 admin.site.register(Schedule, ScheduleAdmin)
 admin.site.register(OpenTime, OpenTimeAdmin)
+admin.site.register(Category, CategoryAdmin)
+
