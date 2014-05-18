17be1cd985aca23cbb3b203def70322201492409
Renfred Harper <renfredxh@gmail.com>
2014-05-18 17:08:08 -0400

    Add rest framework settings

--- whats-open/whats-open/settings/settings.py
+++ whats-open/whats-open/settings/settings.py
@@ -182,3 +182,17 @@ LOGGING = {
 CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = 259200
 CACHE_MIDDLEWARE_KEY_PREFIX = ''
+
+# DJANGO REST FRAMEWORK SETTINGS
+REST_FRAMEWORK = {
+    # Use hyperlinked styles by default.
+    # Only used if the `serializer_class` attribute is not set on a view.
+    'DEFAULT_MODEL_SERIALIZER_CLASS':
+        'rest_framework.serializers.HyperlinkedModelSerializer',
+
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
