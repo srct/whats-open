22ee562ed9125e544e20d53378c3fbed2f5e6e00
Nicholas Anderson <nanderson@nanderson.me>
2015-10-24 16:45:23 -0400

    Now it runs!

--- whats_open/website/urls.py
+++ whats_open/website/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from website.views import *
 from rest_framework.routers import DefaultRouter
+from django.conf.urls.static import static
+from whats_open.settings import local as settings_local
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
@@ -11,4 +13,4 @@ router.register(r'opentimes', OpenTimeViewSet)
 urlpatterns = patterns('website.views',
     url(r'^api/', include(router.urls)),
     url(r'^$', 'facility_grid', name='facility_grid'),
-)
+) + static(settings_local.STATIC_URL, document_root=settings_local.STATIC_ROOT)
