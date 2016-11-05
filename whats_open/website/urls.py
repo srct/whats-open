da5e3117db72b73ea6230020d841b9c73c20d450
Daniel W Bond <dbond2@gmu.edu>
2016-11-05 18:26:21 -0400

    removed duplicated 'website' directory-- byproduct over merging

--- whats_open/website/urls.py
+++ whats_open/website/urls.py
@@ -9,6 +9,20 @@ router.register(r'schedules', ScheduleViewSet)
 router.register(r'opentimes', OpenTimeViewSet)
 
 urlpatterns = patterns('website.views',
+    #url(r'^(?:ajax|api)/schedule/', 'ajax_schedule_data', name='schedule_data'),
+
     url(r'^api/', include(router.urls)),
     url(r'^$', 'facility_grid', name='facility_grid'),
+    #facilities open urls
+    #url(r'^facilities/$', FacilityListView.as_view(), name='faciliites-list'),
+    #url(r'^facilities/(?P<category>)/$', FacilityCategoryListView.as_view(), name='facilities-list-by-cat'),
+    #url(r'^facilities/(?P<slug>)/$', FacilityDetailView.as_view(), name='facilities-detail'),
+    #url(r'^facilities/(?P<on_campus>)/$', FacilityStatusListView.as_view(), name='facilities-list-by-status'),
+
+    #schedules urls
+    #url(r'^schedule/(?P<pk>)/$', ScheduleDetailView.as_view(), name='schedule-detail'),
+
+    #opentime urls
+    url(r'^open-time/(?P<pk>)/$', OpenTimeDetailView.as_view(), name='open-time-detail'),
 )
+
