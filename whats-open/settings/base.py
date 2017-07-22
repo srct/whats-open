e2ed1e43a8f2b6cd5b9af1005d24492dba6423c4
David Haynes <dhaynes3@gmu.edu>
2017-07-22 09:13:15 -0400

    Rate limit anons @ 1000/day
    - easy, thanks DRF

--- whats-open/settings/base.py
+++ whats-open/settings/base.py
@@ -262,7 +262,14 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    # http://www.django-rest-framework.org/api-guide/throttling/#throttling
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1000/day',
+    }
 }
 
 """
